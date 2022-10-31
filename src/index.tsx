b0ed2e4d911ef357d0da2a9954fe7b56442eb284
Subhonbek <basubhonbek@gmail.com>
2022-10-31 23:52:52 +0300

    fix index

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,11 @@ import { Home } from "./pages/Home";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Home />
